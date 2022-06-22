b605bab00f62383773d44ed9143dba60f3a835ad
Mudit Dahiya <mdtheroxtar@gmail.com>
2022-06-21 22:39:38 -0400

    added animatiosn, new pics

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,30 +1,107 @@
-import { useEffect, useTimeout } from "react";
+import { useEffect } from "react";
+import code from "./code.jpeg";
+import guitar from "./guitar.jpg";
+import math from "./math.jpg";
 
 function Home() {
+  //left side
   useEffect(() => {
-    const typingAnimation = (text) => {
-      if (text) {
-        document.getElementById("animateText").innerHTML += text[0];
-      }
+    const texts = [
+      "I am a software developer ...",
+      "I love mathematics ...",
+      "I enjoy playing the guitar ...",
+    ];
+
+    const images = [code, math, guitar];
+
+    const writeText = (text, image) => {
+      return new Promise((resolve, reject) => {
+        const time = 60;
+        for (let i = 0; i < text.length; i++) {
+          setTimeout(() => {
+            document.getElementById("animateText").innerHTML += text[i];
+          }, i * time);
+        }
+
+        document.getElementById("animateImage").src = image;
+
+        setTimeout(() => {
+          let i = 0;
+          const imageFadeIn = setInterval(() => {
+            if (i <= 1) {
+              document.getElementById("animateImage").style.opacity = i;
+              i += 0.04;
+            } else {
+              clearInterval(imageFadeIn);
+            }
+          }, 10);
+        }, time * 5);
+
+        setTimeout(() => {
+          resolve();
+        }, (text.length + 20) * time);
+      });
     };
-    typingAnimation("am a software developer ...");
+
+    const deleteText = (text, image) => {
+      return new Promise((resolve, reject) => {
+        const time = 60;
+        for (let i = 0; i < text.length; i++) {
+          setTimeout(() => {
+            document.getElementById("animateText").innerHTML = document
+              .getElementById("animateText")
+              .innerHTML.slice(0, -1);
+          }, i * time);
+        }
+
+        setTimeout(() => {
+          let i = 1;
+          const imageFadeOut = setInterval(() => {
+            if (i >= -1) {
+              document.getElementById("animateImage").style.opacity = i;
+              i -= 0.04;
+            } else {
+              clearInterval(imageFadeOut);
+            }
+          }, 10);
+        }, (text.length * time) / 2);
+
+        setTimeout(() => {
+          resolve();
+        }, (text.length + 5) * time);
+      });
+    };
+
+    async function animate() {
+      let i = 0;
+      while (true) {
+        await writeText(texts[i], images[i]);
+        await deleteText(texts[i], images[i]);
+        i++;
+        if (i === 3) {
+          i = 0;
+        }
+      }
+    }
+
+    animate();
   });
+
   return (
     <div className="Home">
       <h1>Hi, this is Mudit</h1>
       <div id="stage">
         <div className="leftStage">
           <p>
-            I <span id="animateText"></span>
+            <span id="animateText"></span>
           </p>
         </div>
         <div className="rightStage">
-          <div id="animateImage">this is right stage</div>
+          <div>
+            <img id="animateImage" alt="support"></img>
+          </div>
         </div>
       </div>
-
-      {/* love mathematics */}
-      {/* enjoy playing the guitar */}
     </div>
   );
 }
