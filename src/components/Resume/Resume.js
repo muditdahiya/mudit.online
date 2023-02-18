462bc8ee2861b6fe92f9f68d2c30c9250cd801e7
muditdahiya <mdtheroxtar@gmail.com>
2023-02-18 01:12:11 -0500

    updated resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,14 +5,18 @@ function Resume() {
       <div className="PDF">
         <object
           className="PDF"
-          data="https://drive.google.com/file/d/1hkVs2cTJnawdDW5WOMfUrBwsMMAAUqK2/preview"
+          data="https://drive.google.com/file/d/16OFTqW7bEU23BFg1aT7H3LRQrbI8JPtj/preview"
           type="application/pdf"
         >
           <div>
             No online PDF viewer installed.{" "}
-            <a href="https://drive.google.com/file/d/1hkVs2cTJnawdDW5WOMfUrBwsMMAAUqK2/preview">
+            <a
+              href="https://drive.google.com/file/d/16OFTqW7bEU23BFg1aT7H3LRQrbI8JPtj/preview"
+              target="_blank"
+              rel="noreferrer"
+            >
               Click here
-            </a>
+            </a>{" "}
             to view PDF.
           </div>
         </object>
